1a46c8536939ff853c4191e074616793d15d6968
Daniel Romero <danielromero.sem@gmail.com>
2024-04-12 01:53:44 +0200

    Add new user property to identity

--- BlazorWebServerDRM/Data/ApplicationUser.cs
+++ BlazorWebServerDRM/Data/ApplicationUser.cs
@@ -5,6 +5,7 @@ namespace BlazorWebServerDRM.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string? Country { get; set; }
     }
 
 }
